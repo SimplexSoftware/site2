4ef2cc4b11fc215313d77c00655c1413aa7a1794
Sergey Mazeev <sergey.mazeev@gmail.com>
2018-09-06 11:57:18 +0300

    Страницы переводов

--- app/subscribe.php
+++ app/subscribe.php
@@ -5,6 +5,7 @@ $email = urldecode($email);
 $email = trim($email);
 if (mail("info@simplex-software.ru", "Подписка на рассылку", "E-mail: ".$email ,"From: info@simplex-software.ru \r\n"))
  {     echo "сообщение успешно отправлено"; 
-} else { 
+} else {
     echo "при отправке сообщения возникли ошибки";
+}
  ?>
